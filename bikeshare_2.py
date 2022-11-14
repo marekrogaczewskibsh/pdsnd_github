348037d4b844dcc323de92ca33b6c142e4084139
Marek Rogaczewski <marek.rogaczewski@bshg.com>
2022-11-14 22:19:37 +0100

    refactor: Update "get_filters" method

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -6,6 +6,13 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+MONTHS = ['all', 'january', 'february', 'march', 'april', 'may', 'june']
+
+DAYS = ['all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+POSSIBLE_ANSWERS = ['month', 'day', 'both', 'not at all']
+
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -16,13 +23,40 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington).
+    # HINT: Use a while loop to handle invalid inputs
+    city = ''
+    city_question = 'Would you like to see data for Chicago, New York, or Washington? '
 
+    while city not in CITY_DATA:
+        city = input(city_question).lower()
 
     # get user input for month (all, january, february, ... , june)
+    month = 'all'
+    filter_question = 'Would you like to filter the data by "month", "day", "both" or "not at all"? '
+    user_filter = ''
 
+    while user_filter not in POSSIBLE_ANSWERS:
+        user_filter = input(filter_question).lower()
+
+    if user_filter in ['month', 'both']:
+        month_question = 'Which month - January, February, March, April, May, or June? '
+        month = ''
+
+        while month not in MONTHS:
+            month = input(month_question).lower()
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
+    day = 'all'
+
+    if user_filter in ['day', 'both']:
+        day_question = 'Which day - Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday? '
+        day = ''
+
+        while day not in DAYS:
+            day = input(day_question).lower()
+
+    print(f'\nPreparing data for: \n\tcity \t- {city.title()}\n\tmonth \t- {month.title()}\n\tday \t- {day.title()}')
 
 
     print('-'*40)
