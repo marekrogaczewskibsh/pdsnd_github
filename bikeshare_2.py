aadbb4b0a8614f9e23e226b54f00c7a3f23df1ae
Marek Rogaczewski <marek.rogaczewski@bshg.com>
2022-11-14 22:00:33 +0100

    docs: Add args to "user_stats" method

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -115,7 +115,13 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """
+    Displays statistics on bikeshare users.
+
+    Args:
+        (dataframe) df - name of the data frame to analyze
+        (str) city - name of the city
+    """
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
